e18df7d912f654cd64742c6213a11ef61a35c203
mlinhou <mlinhou@gmail.com>
2024-01-17 15:07:36 -0600

    changed skills to about me

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -5,7 +5,6 @@ import {ProjectList} from "../helpers/ProjectList";
 function Projects() {
   return (
     <div className="projects">
-      <h1> My Projects</h1>
       <div className="projectList">
         {ProjectList.map((project, idx) => {
           return <ProjectItem id={idx} name={project.name} image={project.image} extraImage={project.extraImage}  />;
